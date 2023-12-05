c31c7e3a290b6f6cf4332e4ec23dcba90f73078a
devyan18 <epache17@gmail.com>
2023-12-05 13:56:39 -0300

    asdasdas

---
+++ app.py
@@ -0,0 +1,17 @@
+# app.py
+from flask import Flask
+import os
+
+from dotenv import load_dotenv
+
+# Cargar variables de entorno desde el archivo .env
+load_dotenv()
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return '¡Hola, mundo! Este es mi primer servidor con Flask.'
+
+if __name__ == '__main__':
+    app.run(debug=True, port=os.getenv('PORT', 5000))
